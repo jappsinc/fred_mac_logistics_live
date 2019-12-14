96fe7125ba84318760620768d11c0288b608860c
Benjamin Francis <siscorule@gmail.com>
2019-12-14 13:39:12 +0100

    live updates updates

---
+++ precache-manifest.3220a56e576b336c5e6eae25c7191f7b.js
@@ -0,0 +1,530 @@
+self.__precacheManifest = [
+  {
+    "revision": "f640c26d85247cc2efb5b5b52e8193e8",
+    "url": "resources/fonts/font-icomoon/Read Me.txt"
+  },
+  {
+    "revision": "21aa26586cd14dc3f4a4d9586424f941",
+    "url": "resources/images/photos/6.jpeg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "5bf1f98f2832884155e3127995b08c65",
+    "url": "resources/images/s.jpg"
+  },
+  {
+    "revision": "18b7dc86fedccf008ac1d11d5ea6e831",
+    "url": "resources/images/temp-company.jpg"
+  },
+  {
+    "revision": "7894bb97252b485162d27f9894afc5c9",
+    "url": "resources/images/avatar.jpg"
+  },
+  {
+    "revision": "49333153315de5d28996",
+    "url": "css/chunk-0ec6b183.8653f15e.css"
+  },
+  {
+    "revision": "fe96b754a4491040dd1c",
+    "url": "css/chunk-1e9412b9.b322784b.css"
+  },
+  {
+    "revision": "fe96b754a4491040dd1c",
+    "url": "js/chunk-1e9412b9.a7c7a6bf.js"
+  },
+  {
+    "revision": "e41fe4342a041007add5",
+    "url": "css/chunk-1ef4acc2.e8ea16b6.css"
+  },
+  {
+    "revision": "e41fe4342a041007add5",
+    "url": "js/chunk-1ef4acc2.a80889fd.js"
+  },
+  {
+    "revision": "6c10a2d9d2a935ffa9ce",
+    "url": "css/chunk-24ec7462.c530228d.css"
+  },
+  {
+    "revision": "6c10a2d9d2a935ffa9ce",
+    "url": "js/chunk-24ec7462.7a0af59e.js"
+  },
+  {
+    "revision": "729cce9e380050c8b961",
+    "url": "js/chunk-2d0cf39d.4b43f229.js"
+  },
+  {
+    "revision": "9c46d8b0b4c674e598b6",
+    "url": "js/chunk-2d0e95df.0ae4e151.js"
+  },
+  {
+    "revision": "51c874d52c0c8f2165d2",
+    "url": "js/chunk-2dd760ad.da2de546.js"
+  },
+  {
+    "revision": "7bffe4fa496703092b04",
+    "url": "js/chunk-3f7a9aae.903add49.js"
+  },
+  {
+    "revision": "4a189f55976c7b855b4f",
+    "url": "css/chunk-40487553.34c90ea2.css"
+  },
+  {
+    "revision": "4a189f55976c7b855b4f",
+    "url": "js/chunk-40487553.4b6b599f.js"
+  },
+  {
+    "revision": "b17e9645780e932a86e0",
+    "url": "css/chunk-4d18adfe.fdf6c21b.css"
+  },
+  {
+    "revision": "b17e9645780e932a86e0",
+    "url": "js/chunk-4d18adfe.55c60e2c.js"
+  },
+  {
+    "revision": "0538ca307d8b6da20cab",
+    "url": "css/chunk-4dff77cc.663c0b9f.css"
+  },
+  {
+    "revision": "0538ca307d8b6da20cab",
+    "url": "js/chunk-4dff77cc.df0fba29.js"
+  },
+  {
+    "revision": "496b17f047d8fa565fcf",
+    "url": "css/chunk-5127c0ac.8d8efe67.css"
+  },
+  {
+    "revision": "496b17f047d8fa565fcf",
+    "url": "js/chunk-5127c0ac.47421ebe.js"
+  },
+  {
+    "revision": "dc7acfd58406984d75ab",
+    "url": "js/chunk-5fd2563a.14840dda.js"
+  },
+  {
+    "revision": "1a1f86f4fcb97d6e92ef",
+    "url": "css/chunk-685a8703.69a07f76.css"
+  },
+  {
+    "revision": "1a1f86f4fcb97d6e92ef",
+    "url": "js/chunk-685a8703.ae8d6d9c.js"
+  },
+  {
+    "revision": "92e7ab98549f8ef6cb4b",
+    "url": "css/chunk-6b9dc54a.0a2c88f1.css"
+  },
+  {
+    "revision": "92e7ab98549f8ef6cb4b",
+    "url": "js/chunk-6b9dc54a.9cdc0cd8.js"
+  },
+  {
+    "revision": "f1c82b7a64420ee5d0b4",
+    "url": "js/chunk-712589a4.4791dc0d.js"
+  },
+  {
+    "revision": "45f4d1ece48ddbe491e6",
+    "url": "css/chunk-7a5e9137.5494947c.css"
+  },
+  {
+    "revision": "45f4d1ece48ddbe491e6",
+    "url": "js/chunk-7a5e9137.06b1f5d0.js"
+  },
+  {
+    "revision": "79e9536071e9921f3629",
+    "url": "js/chunk-7a8f1238.1dc058c7.js"
+  },
+  {
+    "revision": "f4b1cc99923a973bd20a",
+    "url": "js/chunk-e2bb3392.7c6a2706.js"
+  },
+  {
+    "revision": "d9b0da2ee073151cd2f4",
+    "url": "css/chunk-vendors.43c90e4b.css"
+  },
+  {
+    "revision": "d9b0da2ee073151cd2f4",
+    "url": "js/chunk-vendors.a5a4565b.js"
+  },
+  {
+    "revision": "66a054cd3dbb6c2a0f5e04b5c7b3be78",
+    "url": "index.html"
+  },
+  {
+    "revision": "c5f20ee1054e12e8d453f7a14234a557",
+    "url": "custom.css"
+  },
+  {
+    "revision": "7fec23ef95e17ff623af5dd5db0fc87d",
+    "url": "resources/fonts/font-awesome/less/animated.less"
+  },
+  {
+    "revision": "da355b2c884a067eedd979b445968147",
+    "url": "resources/fonts/font-awesome/less/core.less"
+  },
+  {
+    "revision": "6799c9cf7edf54b7432cca85854b0abf",
+    "url": "resources/fonts/font-awesome/less/fixed-width.less"
+  },
+  {
+    "revision": "62ab0957ccd3369ed07a6d7241e7b9c1",
+    "url": "resources/fonts/font-awesome/less/font-awesome.less"
+  },
+  {
+    "revision": "f588164c1a95535137186bbdb0d236c8",
+    "url": "resources/fonts/font-awesome/less/larger.less"
+  },
+  {
+    "revision": "abcbffd56677821190cf4dc72f660dd9",
+    "url": "resources/fonts/font-awesome/less/list.less"
+  },
+  {
+    "revision": "e5f4d96ed70c0c372ccf7a0d9841ba53",
+    "url": "resources/fonts/font-awesome/HELP-US-OUT.txt"
+  },
+  {
+    "revision": "5b203b320a03575de5f369cf49b1e7c2",
+    "url": "resources/fonts/font-awesome/less/mixins.less"
+  },
+  {
+    "revision": "1f57858ac2a7ab59b01088c778f5fb0a",
+    "url": "resources/fonts/font-awesome/less/bordered-pulled.less"
+  },
+  {
+    "revision": "fc4e5dec2b323ba7f687b0414a6faecd",
+    "url": "resources/fonts/font-awesome/less/path.less"
+  },
+  {
+    "revision": "2088e18f446fcf2fe91e5293fc7f2847",
+    "url": "resources/fonts/font-awesome/less/rotated-flipped.less"
+  },
+  {
+    "revision": "79f38eb1f3eea913308f49ab5cd663bf",
+    "url": "resources/fonts/font-awesome/less/screen-reader.less"
+  },
+  {
+    "revision": "6e194951ee06dd4cde6f94ed2ec86e47",
+    "url": "resources/fonts/font-awesome/scss/_bordered-pulled.scss"
+  },
+  {
+    "revision": "d4d62bafcfb00eb1b208acbf895a95a3",
+    "url": "resources/fonts/font-awesome/scss/_core.scss"
+  },
+  {
+    "revision": "79db4428659752f59630c4388bbaa174",
+    "url": "resources/fonts/font-awesome/scss/_animated.scss"
+  },
+  {
+    "revision": "e6fac8be09e2d3dc38fe9248b2eb0472",
+    "url": "resources/fonts/font-awesome/scss/_fixed-width.scss"
+  },
+  {
+    "revision": "8b7e28e004c785a0a1a73afde547b9e7",
+    "url": "resources/fonts/font-awesome/less/stacked.less"
+  },
+  {
+    "revision": "8b00a4d732107fc1844ea18314c3cff9",
+    "url": "resources/fonts/font-awesome/scss/_larger.scss"
+  },
+  {
+    "revision": "c33b069275c5877a4b3f144684664bf5",
+    "url": "resources/fonts/font-awesome/scss/_list.scss"
+  },
+  {
+    "revision": "9a3f214edda562bf122802da5c686a12",
+    "url": "resources/fonts/font-awesome/scss/_rotated-flipped.scss"
+  },
+  {
+    "revision": "b4b497bdd83f580fa3942390763cc270",
+    "url": "resources/fonts/font-awesome/scss/_screen-reader.scss"
+  },
+  {
+    "revision": "638e8ae84e80a3428e9446578a7ed6a0",
+    "url": "resources/fonts/font-awesome/scss/_stacked.scss"
+  },
+  {
+    "revision": "123f2994215b526902be3fa0596c8d13",
+    "url": "resources/fonts/font-awesome/scss/_path.scss"
+  },
+  {
+    "revision": "5713ae44ff4338acad6059ee69f6286b",
+    "url": "resources/fonts/font-icomoon/demo-files/demo.css"
+  },
+  {
+    "revision": "b1316a24b0be134a80fe424883c5dbab",
+    "url": "resources/fonts/font-icomoon/demo-files/demo.js"
+  },
+  {
+    "revision": "bc5096695c4092d06d77da5329254590",
+    "url": "resources/fonts/font-awesome/scss/font-awesome.scss"
+  },
+  {
+    "revision": "f07164e3950c5dac464e3f5f9e14858d",
+    "url": "resources/fonts/font-awesome/scss/_mixins.scss"
+  },
+  {
+    "revision": "18b3b9eda5142e2e7d29",
+    "url": "js/app.18bbab7f.js"
+  },
+  {
+    "revision": "e8256dc2d3719135acf865403156a6d8",
+    "url": "resources/fonts/font-linearicons/demo-files/demo.css"
+  },
+  {
+    "revision": "49333153315de5d28996",
+    "url": "js/chunk-0ec6b183.e0691682.js"
+  },
+  {
+    "revision": "b1316a24b0be134a80fe424883c5dbab",
+    "url": "resources/fonts/font-linearicons/demo-files/demo.js"
+  },
+  {
+    "revision": "eb5859aeb57953d4afce2c0ad7d3d2da",
+    "url": "resources/images/ecommerce/ecommerce-empty.jpg"
+  },
+  {
+    "revision": "eb7ee52b41e1340be3a1d0d01bb8d84f",
+    "url": "resources/images/mediatec.png"
+  },
+  {
+    "revision": "39275c96a3d1c313f968b0c8ce13cd19",
+    "url": "resources/fonts/font-linearicons/Read Me.txt"
+  },
+  {
+    "revision": "8b591512a23e5320a5cbf496100ef3e9",
+    "url": "resources/images/icons/icon-next-step.svg"
+  },
+  {
+    "revision": "7894bb97252b485162d27f9894afc5c9",
+    "url": "resources/images/avatars/temp.jpg"
+  },
+  {
+    "revision": "0e4453b10c71ab047d2a990ca5b38cef",
+    "url": "resources/images/n.jpg"
+  },
+  {
+    "revision": "5c6a48d7333bf7d6f34d3f9a8835b224",
+    "url": "resources/images/mediatec-inverse.png"
+  },
+  {
+    "revision": "22a088a59532b68572a7",
+    "url": "js/chunk-0b610f05.99716add.js"
+  },
+  {
+    "revision": "05c2671937e0e3c0a55da0d5d0d5d58e",
+    "url": "resources/images/photos/1.jpeg"
+  },
+  {
+    "revision": "811880d022aeaa00641848f97e894294",
+    "url": "resources/images/avatars/2.jpg"
+  },
+  {
+    "revision": "ceb953950114055e05c4",
+    "url": "js/chunk-07cf1f60.c3f6e9af.js"
+  },
+  {
+    "revision": "a1b3238ccfd5361188f56b9c58dbaec1",
+    "url": "resources/images/avatars/1.jpg"
+  },
+  {
+    "revision": "a21d880d6e689542d046505aac9ed5c3",
+    "url": "resources/fonts/font-linearicons/style.css"
+  },
+  {
+    "revision": "68e26b0a868cd5bec4ef4b10e16008e8",
+    "url": "resources/images/avatars/5.jpg"
+  },
+  {
+    "revision": "04e646fbfac9bb1e06b6f80edf981576",
+    "url": "resources/images/avatars/4.jpg"
+  },
+  {
+    "revision": "1d8bf037bf8efc66194b6719355dede8",
+    "url": "resources/images/avatars/3.jpg"
+  },
+  {
+    "revision": "62803119d3ed114705a50717a53e2639",
+    "url": "resources/fonts/font-awesome/less/variables.less"
+  },
+  {
+    "revision": "547c9f94a31ced29cbc7f029d700a639",
+    "url": "resources/fonts/font-awesome/scss/_variables.scss"
+  },
+  {
+    "revision": "03e91f122aa5fd425abbe23c85546eb0",
+    "url": "resources/fonts/font-linearicons/fonts/Linearicons-Free.woff2"
+  },
+  {
+    "revision": "2f19b9c9d492f8107fe26bd31bdfda5f",
+    "url": "resources/images/amazon.jpg"
+  },
+  {
+    "revision": "2f19b9c9d492f8107fe26bd31bdfda5f",
+    "url": "resources/images/ecommerce/amazon.jpg"
+  },
+  {
+    "revision": "a0e784c4ca94c271b0338dfb02055be6",
+    "url": "resources/fonts/font-awesome/css/font-awesome.min.css"
+  },
+  {
+    "revision": "8ea705229934b79f794575eef610f8e7",
+    "url": "resources/fonts/font-icomoon/style.css"
+  },
+  {
+    "revision": "e692cc055b4a1628c270259916d91073",
+    "url": "resources/images/ecommerce/002.jpg"
+  },
+  {
+    "revision": "12d20a7840af0a058bb3986ba71f8c81",
+    "url": "resources/images/ecommerce/003.jpg"
+  },
+  {
+    "revision": "df136b8118006b8157df81699b68147e",
+    "url": "resources/images/ecommerce/001.jpg"
+  },
+  {
+    "revision": "6adc63fabea640e4292938afa093f06f",
+    "url": "resources/images/ecommerce/004.jpg"
+  },
+  {
+    "revision": "4bb3dd721c4652feee0953261d329710",
+    "url": "resources/fonts/font-awesome/css/font-awesome.css"
+  },
+  {
+    "revision": "e3f226765542c7b9de0e9bc8d713c6ce",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "e3f226765542c7b9de0e9bc8d713c6ce",
+    "url": "logos/Fred_Mac_sm_.png"
+  },
+  {
+    "revision": "0ccc55576d78b16633a36ff724bf998d",
+    "url": "favicon1.png"
+  },
+  {
+    "revision": "e624b0b5cfd7ac3fcb4eefab15f983f6",
+    "url": "resources/fonts/font-awesome/scss/_icons.scss"
+  },
+  {
+    "revision": "ddaa02ea18934d4e03c9978f13311d5e",
+    "url": "resources/fonts/font-awesome/less/icons.less"
+  },
+  {
+    "revision": "b9b7f23cb61b1f503e1249b63d980448",
+    "url": "resources/fonts/font-linearicons/fonts/Linearicons-Free.eot"
+  },
+  {
+    "revision": "2f3e9f80fff7d699dd3de6904d7d1647",
+    "url": "resources/fonts/font-linearicons/fonts/Linearicons-Free.ttf"
+  },
+  {
+    "revision": "65060723fe964f85afa0a82d0bb78cf9",
+    "url": "resources/fonts/font-linearicons/fonts/Linearicons-Free.woff"
+  },
+  {
+    "revision": "0ccc55576d78b16633a36ff724bf998d",
+    "url": "resources/images/logo-inverse-mobile.png"
+  },
+  {
+    "revision": "0ccc55576d78b16633a36ff724bf998d",
+    "url": "resources/images/logo-mobile.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "resources/fonts/font-awesome/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "65ff8dcfab45dc8bab8e69949138f14f",
+    "url": "resources/images/logo.png"
+  },
+  {
+    "revision": "2f5e8c9e2a9673f418650a462b140361",
+    "url": "resources/images/logo-inverse.png"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "resources/fonts/font-awesome/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "4ada53ea9a28098dbb64ae36262d2ddf",
+    "url": "resources/fonts/font-icomoon/fonts/icomoon.eot"
+  },
+  {
+    "revision": "ddd818f6c1d4cc8bcdf83cbd56eb04d2",
+    "url": "resources/fonts/font-icomoon/fonts/icomoon.ttf"
+  },
+  {
+    "revision": "2ff40c51ead6992381376ececf1222f7",
+    "url": "resources/fonts/font-icomoon/fonts/icomoon.woff"
+  },
+  {
+    "revision": "e60e32d9e24e46d7258fcbb99664162f",
+    "url": "resources/fonts/font-linearicons/demo.html"
+  },
+  {
+    "revision": "0d2717cd5d853e5c765ca032dfd41a4d",
+    "url": "resources/fonts/font-awesome/fonts/FontAwesome.otf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "resources/fonts/font-awesome/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "resources/fonts/font-awesome/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "ec161a74448ccf2c18beefe8cca2b44a",
+    "url": "resources/fonts/font-linearicons/fonts/Linearicons-Free.svg"
+  },
+  {
+    "revision": "70c56c554f0a1f0d609cc3a27c1008e8",
+    "url": "resources/fonts/font-linearicons/selection.json"
+  },
+  {
+    "revision": "5244765b2917ee536d2ddcd068cff10b",
+    "url": "resources/fonts/font-icomoon/fonts/icomoon.svg"
+  },
+  {
+    "revision": "9307b365743fa322878d28334d28e5a6",
+    "url": "resources/images/photos/5.jpeg"
+  },
+  {
+    "revision": "3ac4b5ec4016d1a55d5221d2e9926c99",
+    "url": "logos/Fred_Mac.png"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "resources/fonts/font-awesome/fonts/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "e4c51230485b5f94e8a7955beb4508e3",
+    "url": "resources/fonts/font-icomoon/selection.json"
+  },
+  {
+    "revision": "901aa76e56fec9950da7be5cdbf7526d",
+    "url": "resources/images/photos/4.jpeg"
+  },
+  {
+    "revision": "1cf746b8c96f94178fc139976cfe4a2c",
+    "url": "resources/fonts/font-icomoon/demo.html"
+  },
+  {
+    "revision": "f658e8d5248b69c8852f9bb5e01be121",
+    "url": "resources/images/photos/3.jpeg"
+  },
+  {
+    "revision": "8a4639d279c006d26643b8f3a90ecc67",
+    "url": "resources/images/photos/2.jpeg"
+  },
+  {
+    "revision": "22a088a59532b68572a7",
+    "url": "css/chunk-0b610f05.7e1520cc.css"
+  },
+  {
+    "revision": "18b3b9eda5142e2e7d29",
+    "url": "css/app.b6251602.css"
+  }
+];
